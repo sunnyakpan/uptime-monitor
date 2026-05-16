f59af93ddef4e1c0f3f47b628dca298c7374f9f8
SunnyAkpan <divinesunnyakpan@gmail.com>
2026-05-16 21:16:55 +0100

    Added  API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\MonitorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/monitors', [MonitorController::class, 'store']);
+Route::get('/monitors', [MonitorController::class, 'index']);
+Route::get('/monitors/{id}/history', [MonitorController::class, 'history']);
